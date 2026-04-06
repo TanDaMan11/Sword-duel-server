cc93798c402173473636af711d12febef5f3ac39
TanDaMan11 <tanner.scarlett@hotmail.com>
2026-04-06 11:36:53 -0700

    fix joining

--- server.js
+++ server.js
@@ -18,14 +18,16 @@ function generateCode() {
 }
 
 io.on("connection", (socket) => {
-  console.log("Player connected");
+  console.log("Player connected:", socket.id);
 
   socket.on("create_room", (data) => {
     const code = data.code;
     rooms[code] = {
-      players: [socket.id]
+      players: [socket.id],
+      gameOver: false
     };
     socket.join(code);
+    console.log(`Room ${code} created by ${socket.id}`);
     socket.emit("room_created", { code });
   });
 
@@ -34,19 +36,85 @@ io.on("connection", (socket) => {
     if (rooms[code] && rooms[code].players.length < 2) {
       rooms[code].players.push(socket.id);
       socket.join(code);
+      console.log(`Player ${socket.id} joined room ${code}`);
       socket.emit("room_joined", { code });
+      // Notify the host that a player joined
       socket.to(code).emit("player_joined");
     } else {
       socket.emit("error", { message: "Room full or not found" });
     }
   });
 
+  socket.on("start_game", (data) => {
+    const code = data.room;
+    if (rooms[code]) {
+      console.log(`Game starting in room ${code}`);
+      // Send game_start to both players
+      io.to(code).emit("game_start");
+    }
+  });
+
   socket.on("game_state", (data) => {
-    socket.to(data.room).emit("game_state", data);
+    const code = data.room;
+    if (rooms[code]) {
+      // Send the game state to the other player
+      socket.to(code).emit("game_state", {
+        p1: data.playerId === 1 ? {
+          x: data.x,
+          y: data.y,
+          vx: data.vx,
+          vy: data.vy,
+          health: data.health,
+          attacking: data.attacking,
+          facingRight: data.facingRight
+        } : null,
+        p2: data.playerId === 2 ? {
+          x: data.x,
+          y: data.y,
+          vx: data.vx,
+          vy: data.vy,
+          health: data.health,
+          attacking: data.attacking,
+          facingRight: data.facingRight
+        } : null
+      });
+    }
+  });
+
+  socket.on("game_over", (data) => {
+    const code = data.room;
+    if (rooms[code]) {
+      console.log(`Game over in room ${code}. Winner: ${data.winner}`);
+      rooms[code].gameOver = true;
+      // Notify both players
+      io.to(code).emit("game_over", { winner: data.winner });
+    }
+  });
+
+  socket.on("rematch_ready", (data) => {
+    const code = data.room;
+    if (rooms[code]) {
+      console.log(`Player ${socket.id} is ready for rematch in room ${code}`);
+      // Notify the other player that this player is ready
+      socket.to(code).emit("rematch_ready");
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("Player disconnected");
+    console.log("Player disconnected:", socket.id);
+    // Notify other players in the room that this player left
+    for (const code in rooms) {
+      if (rooms[code].players.includes(socket.id)) {
+        rooms[code].players = rooms[code].players.filter(id => id !== socket.id);
+        io.to(code).emit("player_left");
+        // Clean up empty rooms
+        if (rooms[code].players.length === 0) {
+          delete rooms[code];
+          console.log(`Room ${code} deleted`);
+        }
+        break;
+      }
+    }
   });
 });
 
